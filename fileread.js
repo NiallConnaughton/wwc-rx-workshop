e67b3009aea7a54472d9c21bc3dd548971be44c4
Niall Connaughton <nconnaughton@gmail.com>
2015-04-11 09:40:05 +1000

    Adding stop and start of tweets per minute code

--- fileread.js
+++ fileread.js
@@ -26,61 +26,79 @@ function runTweetStream() {
         split = text.split('\r\n');
 
         startTime = 0;
+        //tweets = split
+        //            .filter(function (line) { return line !== ""; })
+        //            .map(function (json) {
+        //                tweet = JSON.parse(json);
+        //                tweet.timestamp = Date.parse(tweet.CreatedAt);
+
+        //                //if (!startTime)
+        //                //    startTime = tweet.timestamp;
+
+        //                tweet.timeOffset = tweet.timestamp - startTime;
+
+        //                return tweet;
+        //            });
+
         tweets = split
-                    .filter(function (line) { return line !== ""; })
-                    .map(function (json) {
-                        tweet = JSON.parse(json);
-                        tweet.timestamp = Date.parse(tweet.CreatedAt);
+            .filter(function (line) { return line !== ""; })
+            .map(function (json) {
+                tweet = JSON.parse(json);
+                tweet.timestamp = Date.parse(tweet.CreatedAt);
+                return tweet;
+            });
+        
+        startTime = tweets[0].timestamp;
+        tweets.forEach(function (tweet) {
+            tweet.timeOffset = tweet.timestamp - startTime;
+        });
 
-                        if (!startTime)
-                            startTime = tweet.timestamp;
-
-                        tweet.timeOffset = tweet.timestamp - startTime;
-
-                        return tweet;
-                    });
         //tweets.subscribe(function (t) { console.log(t); });
 
         var scheduler = new Rx.VirtualTimeScheduler(0, comparer);
         scheduler.add = addSchedulerTime;
         scheduler.toDateTimeOffset = toDateTimeOffset;
         scheduler.toRelative = toRelative;
-        //scheduler = new Rx.TestScheduler();
         scheduler.advanceTo(startTime);
 
-        var tweetStream = Rx.Observable.fromArray(tweets)
-                            //.take(100)
-                            .map(function (t) {
-                                //return Rx.Observable.timer(t.timestamp, 0, scheduler);
-                                return Rx.Observable.return(t).delay(t.timeOffset, scheduler);
-                            })
-                            .mergeAll();
+        var tweetStream = Rx.Observable.for(tweets, function (t) {
+            return Rx.Observable.timer(new Date(t.timestamp), scheduler)
+                                .map(function () { return t; });
+        });
 
-        console.log('Tweet stream created.');
+        tweetSubscription = new Rx.CompositeDisposable();
 
-        tweetStream.subscribe(function (t) {
+        tweetSubscription.add(tweetStream.subscribe(function (t) {
             console.log(new Date(t.timestamp) + " - " + t.ScreenName);
-        });
+        }));
 
-        console.log('Subscribed');
+        tweetSubscription.add(
+            Rx.Observable.interval(1000)
+                         .subscribe(function() {
+                                            scheduler.advanceBy(1000 * multiplier);
+                                            nowSpan.innerText = new Date(scheduler.now());
+                         }));
 
-        //historicalScheduler.start();
-        Rx.Observable.interval(1000).subscribe(function (foo) {
-            //multiplier = timeMultiplier.value;
-            scheduler.advanceBy(1000 * multiplier);
-            nowSpan.innerText = new Date(scheduler.now());
-        });
+        var tweetsPerMinute = tweetStream.window(Rx.Observable.timer(60000, scheduler))
+                   .map(function (minuteTweet) {
+                       return minuteTweet.count();
+                   })
+                   .mergeAll();
 
-        console.log('Subscribed to scheduler interval');
+        tweetSubscription.add(tweetsPerMinute.subscribe(function (tpm) {
+            tpmSpan.innerText = tpm;
+            foo;
+        }));
 
-        //scheduler.start();
-
-        console.log(new Date(startTime));
+        console.log('Tweet stream started.');
     };
-    //var slice = file.slice(0, 10000);
-    //filereader.readAsText(slice);
 
     filereader.readAsText(file);
+}
+
+function stopTweetStream() {
+    tweetSubscription.dispose();
+    console.log('Tweet stream stopped.');
 }
 
 function multiplierChanged() {
@@ -97,6 +115,11 @@ function setupEventHandlers() {
 
     var goButton = document.getElementById('goButton');
     goButton.onclick = runTweetStream;
+
+    var stopButton = document.getElementById('stopButton');
+    stopButton.onclick = stopTweetStream;
+
+    tpmSpan = document.getElementById('tpm');
 }
 
 window.onload = setupEventHandlers;
